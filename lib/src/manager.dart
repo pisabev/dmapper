2423fb6b5c8b3de9f2cc0d35d30a962b9d3d7f87
user <pisabev@gmail.com>
2014-06-18 04:38:24 +0300

    auto

--- lib/src/manager.dart
+++ lib/src/manager.dart
@@ -107,6 +107,8 @@ class Manager<A extends Application> {
         return new Future.sync(() {
             if(_unit.started)
                 return _unit.rollback().then((_) => connection.close());
+            _unit = new Unit(this);
+            _cache = new Cache();
             return connection.close();
         });
     }
