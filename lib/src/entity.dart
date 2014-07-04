935841f3df9939960d0d9648bb77815bc0e9a20e
user <pisabev@gmail.com>
2014-07-04 05:03:22 +0300

    auto

--- lib/src/entity.dart
+++ lib/src/entity.dart
@@ -1,8 +1,14 @@
 part of mapper;
 
-abstract class Entity<E> {
+class Entity {
 
     void init(Map data);
+
+    void initMerge(Map data) {
+        var m = toMap();
+        m.addAll(data);
+        init(m);
+    }
 
     Map toMap();
 
